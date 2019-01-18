22fb58a564954240f9b91d13d43cf45328c3eb6c
Mohammad Mahjoub <mmahjoub@westmont.edu>
2019-01-17 23:22:18 -0800

    adding setup.

---
+++ setup.py
@@ -0,0 +1,13 @@
+from setuptools import setup
+
+setup(
+    name='Real Estate Investment Calculator',
+    version='0.0.1',
+    description='a pip-installable package example',
+    license='MIT',
+    packages=['/Users/mohammad/PycharmProjects/wealth-maneger-CLT'],
+    author='Mohammad Mahjoub',
+    author_email='youvsyou47@gmail.com',
+    keywords=['example'],
+    url='https://github.com/Hyped-247/wealth-maneger-CLT.git'
+)
