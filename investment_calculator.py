9b8261c70ff295791a93798f09e353ec60110c4a
Mohammad Mahjoub <mmahjoub@westmont.edu>
2019-01-18 22:37:23 -0800

    deleting an object.

--- investment_calculator.py
+++ investment_calculator.py
@@ -78,10 +78,5 @@ class Calculator:
               .format(self._passive_income_desired_yearly, self._starting_year - self._years_needed))
         print("You have to invest ${0} every year ".format(self._yearly_savings))
         print("")
-
-
-
-
-    
     
 
